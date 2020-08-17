db63b1f3d51f8ae185b2451b331155af63bfa50a
王立广 <wangliguang@kaikeba.com>
2020-08-17 18:22:24 +0800

    页面布局

--- screens/FlexBoxPage.tsx
+++ screens/FlexBoxPage.tsx
@@ -1,15 +1,68 @@
 import React from 'react';
 import {
     Text,
-    View
+    View,
+    StyleSheet
 } from 'react-native';
+
+/**
+ * flex
+ * flexDirection
+ * justifyContent
+ * alignItems
+ * alignSelf
+ * flexWrap
+ *
+ */
 
 export default class extends React.Component {
     render() {
         return (
-            <View>
-                <Text>adfasdfadf</Text>
+            <View style={STYLE.page}>
+                <View style={{ marginBottom: 40 }}>
+                    <View style={[STYLE.item, STYLE.red]}/>
+                    <View style={[STYLE.item, STYLE.green]}/>
+                    <View style={[STYLE.item, STYLE.blue]}/>
+                </View>
+
+                <View style={{ marginBottom: 40, flexDirection: 'row'}}>
+                    <View style={[STYLE.item, STYLE.red]}/>
+                    <View style={[STYLE.item, STYLE.green]}/>
+                    <View style={[STYLE.item, STYLE.blue]}/>
+                </View>
+
+                <View style={{ marginBottom: 40, flexDirection: 'row', justifyContent: 'center'}}>
+                    <View style={[STYLE.item, STYLE.red]}/>
+                    <View style={[STYLE.item, STYLE.green]}/>
+                    <View style={[STYLE.item, STYLE.blue]}/>
+                </View>
+
+                <View style={{ marginBottom: 40, flexDirection: 'row', justifyContent: 'flex-end'}}>
+                    <View style={[STYLE.item, STYLE.red]}/>
+                    <View style={[STYLE.item, STYLE.green]}/>
+                    <View style={[STYLE.item, STYLE.blue]}/>
+                </View>
             </View>
         );
     }
 }
+
+const STYLE = StyleSheet.create({
+    page: {
+        // flex: 1,
+    },
+    red: {
+        backgroundColor: 'orange',
+    },
+    blue: {
+        backgroundColor: 'blue',
+    },
+    green: {
+        backgroundColor: 'green',
+    },
+    item: {
+        width: 70,
+        height: 70,
+        backgroundColor: 'orange',
+    }
+});
